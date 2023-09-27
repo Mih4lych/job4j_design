7715c84b4c69d36c8ba11aeafa87f02282043cef
Mih4lych <mih4illl@gmail.com>
2023-09-27 07:20:18 +0300

    5. Утверждения с коллекциями [#504885]

---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> list = simpleConvert.toList("first", "second", "three", "four", "five");
+        assertThat(list).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+        assertThat(list).filteredOn(s -> s.startsWith("f"))
+                .hasSize(3)
+                .last()
+                .isEqualTo("five");
+    }
+
+    @Test
+    void checkSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Set<String> set = simpleConvert.toSet("first", "second", "three", "four", "five", "five");
+        assertThat(set).contains("second")
+                .hasSizeLessThan(6)
+                .containsAnyOf("zero", "second", "six")
+                .allSatisfy(s -> {
+                    assertThat(s).hasSizeLessThan(7);
+                    assertThat(s).isNotEqualTo("ten");
+                });
+    }
+
+    @Test
+    void checkMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> map = simpleConvert.toMap("first", "second", "three", "four", "five" );
+        assertThat(map).hasSize(5)
+                .containsKeys("first", "five")
+                .containsValues(0, 4)
+                .containsEntry("second", 1);
+    }
+}
