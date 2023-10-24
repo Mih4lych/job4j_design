c1f0aa1d9f5ba45aa7f76ecc2b0ab9a75975f0a8
Mih4lych <mih4illl@gmail.com>
2023-10-24 08:52:12 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            out.write("Hello, world!".getBytes());
+            out.write(System.lineSeparator().getBytes());
+
+            for (int i = 0; i <= 10; i++) {
+                for (int j = 0; j <= 10; j++) {
+                    out.write((i + " * " + j + " = " + (i * j)).getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
