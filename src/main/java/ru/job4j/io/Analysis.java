34ac2727113c26ab8414c8f848bd2626d5e0dc82
Mih4lych <mih4illl@gmail.com>
2023-11-02 10:16:08 +0300

    7. Анализ доступности сервера [#859 #456917]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,39 +1,30 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
                 BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            List<String> results = new LinkedList<>();
             StringBuilder intermediateResult = new StringBuilder();
+            String data;
+            String[] pair;
 
-            reader
-                    .lines()
-                    .map(s -> s.split(" "))
-                    .forEach(pair -> {
-                        if (intermediateResult.length() == 0) {
-                            intermediateResult.append("400".equals(pair[0]) || "500".equals(pair[0]) ? pair[1] + ";" : "");
-                        } else {
-                            if (!("400".equals(pair[0]) || "500".equals(pair[0]))) {
-                                intermediateResult.append(pair[1]).append(";");
-                                results.add(intermediateResult.toString());
-                                intermediateResult.setLength(0);
-                            }
-                        }
-                    });
-
-            if (intermediateResult.length() > 0) {
-                results.add(intermediateResult.toString());
+            while ((data = reader.readLine()) != null) {
+                pair = data.split(" ");
+                if (intermediateResult.length() == 0) {
+                    intermediateResult.append("400".equals(pair[0]) || "500".equals(pair[0]) ? pair[1] + ";" : "");
+                } else {
+                    if (!("400".equals(pair[0]) || "500".equals(pair[0]))) {
+                        intermediateResult.append(pair[1]).append(";");
+                        writer.write(intermediateResult.toString());
+                        intermediateResult.setLength(0);
+                    }
+                }
             }
 
-            for (String s : results) {
-                writer.write(s + System.lineSeparator());
+            if (intermediateResult.length() > 0) {
+                writer.write(intermediateResult.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
