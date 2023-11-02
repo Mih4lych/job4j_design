b140dafb7481f0436994f5f1e55c321397624e8c
Mih4lych <mih4illl@gmail.com>
2023-11-02 11:10:37 +0300

    7. Анализ доступности сервера [#859 #456917] (fix the solution).

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,25 +6,19 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
                 BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            StringBuilder intermediateResult = new StringBuilder();
             String data;
             String[] pair;
+            boolean isStopped = false;
 
             while ((data = reader.readLine()) != null) {
                 pair = data.split(" ");
-                if (intermediateResult.isEmpty()) {
-                    intermediateResult.append("400".equals(pair[0]) || "500".equals(pair[0]) ? pair[1] + ";" : "");
-                } else {
-                    if (!("400".equals(pair[0]) || "500".equals(pair[0]))) {
-                        intermediateResult.append(pair[1]).append(";");
-                        writer.write(intermediateResult + System.lineSeparator());
-                        intermediateResult.setLength(0);
-                    }
+                if (isStopped && !("400".equals(pair[0]) || "500".equals(pair[0]))) {
+                    writer.write(pair[1] + ";" + System.lineSeparator());
+                    isStopped = false;
+                } else if (!isStopped && ("400".equals(pair[0]) || "500".equals(pair[0]))) {
+                    writer.write(pair[1] + ";");
+                    isStopped = true;
                 }
-            }
-
-            if (intermediateResult.length() > 0) {
-                writer.write(intermediateResult.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
